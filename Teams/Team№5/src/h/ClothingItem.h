1138cbe453178b69bb6c337ed30bd823f82abb50
DaniellaGeorgieva <97393225+DaniellaGeorgieva@users.noreply.github.com>
2022-06-11 20:23:14 +0300

    updated base class

--- Teams/Team№5/src/h/ClothingItem.h
+++ Teams/Team№5/src/h/ClothingItem.h
@@ -1,1 +1,39 @@
+#pragma once
+#include"String.h"
 
+class ClothingItem {
+protected:
+	enum Gender {
+		MAN,
+		WOMAN,
+		UNISEX
+	} gender;
+	enum Type {
+		Casual,
+		Sportswear,
+		Couture
+	} type;
+private:
+
+	String name;
+	String size;
+	double price;
+
+public:
+	ClothingItem();
+	ClothingItem(const String&, const String&, const double);
+
+	virtual ~ClothingItem() = default;
+
+	void setName(const String&);
+	void setSize(const String&);
+	void setPrice(const double);
+
+	String& getName() const;
+	String& getSize() const;
+	double getPrice() const;
+
+	virtual ClothingItem* clone() const = 0;
+	virtual void print() const;
+
+};
