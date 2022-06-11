64309decbdc062f43ad9f482eae2e8d2cbb79310
DaniellaGeorgieva <97393225+DaniellaGeorgieva@users.noreply.github.com>
2022-06-11 21:08:46 +0300

    upload base cpp

--- Teams/Team№5/src/cpp/ClothingItem.cpp
+++ Teams/Team№5/src/cpp/ClothingItem.cpp
@@ -1,1 +1,70 @@
+#include"ClothingItem.h"
+#include<iostream>
 
+ClothingItem::ClothingItem(): name(), price(0), numAvailable(0)
+{}
+ClothingItem::ClothingItem(const String& name, const double price, const size_t numAvailable) : name(name), price(price), numAvailable(numAvailable)
+{}
+
+void ClothingItem::setName(const String& name)
+{
+	this->name = name;
+}
+
+void ClothingItem::setPrice(const double price) 
+{
+	this->price = price;
+}
+void ClothingItem::setNumAvailable(const size_t numAvailable) 
+{
+	this->numAvailable = numAvailable;
+}
+
+Type ClothingItem::getType() const
+{
+	return Type::UNKNOWN;
+}
+Gender ClothingItem::getGender() const
+{
+	return Gender::UNISEX;
+}
+
+Size ClothingItem::getSize() const
+{
+	return Size::UNKNOWN;
+}
+
+String& ClothingItem::getName() const
+{
+	return name;
+}
+
+double ClothingItem::getPrice() const
+{
+	return price;
+}
+size_t ClothingItem::getNumAvailable() const
+{
+	return numAvailable;
+}
+const char* ClothingItem::typeToStr() const
+{
+	return "Unknown";
+}
+const char* ClothingItem::sizeToStr() const
+{
+	return "Unknown";
+}
+const char* ClothingItem::genderToStr() const
+{
+	return "Unisex";
+}
+void ClothingItem::print() const
+{
+	std::cout << "Name: " << name << std::endl
+		<< "Price: " << price << std::endl
+		<< "Size: " << sizeToStr() << std::endl
+		<< "Number available: " << numAvailable << std::endl
+		<< "Type: " << typeToStr() << std::endl
+		<< "Gender: " << genderToStr() << std::endl;
+}
