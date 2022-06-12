a52f159511918ad4df8ca13d0d8bf8ad30484f30
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-12 21:53:40 +0300

    Update ClothingContainer.h

--- Teams/Team№5/src/h/ClothingContainer.h
+++ Teams/Team№5/src/h/ClothingContainer.h
@@ -16,6 +16,7 @@ public:
 	ClothingContainer& operator=(const ClothingContainer&);
 	~ClothingContainer();
 
+	double getPrice(size_t);
 	void AddClothingItem(ClothingItem&);
 	bool DeleteClothingItem(size_t);
 	void ListAvailableItems();
