db882f584113fa17c2a20c3af467d02d6374d9e2
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-12 21:51:02 +0300

    Update Couture.h

--- Teams/Team№5/src/h/Couture.h
+++ Teams/Team№5/src/h/Couture.h
@@ -5,7 +5,7 @@ class Couture : public ClothingItem {
 	String designer;
 public:
 	Couture(const String& name, const double price, const size_t numAvailable);
-	
+	void setDesigner(const String&);
 	virtual Type getType() const override;
 	virtual const char* typeToStr() const override;
 	virtual ClothingItem* clone() const override;
