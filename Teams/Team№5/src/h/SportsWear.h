bc1bb1637216983635137d49151a2a36e494035b
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-12 21:50:26 +0300

    Update SportsWear.h

--- Teams/Team№5/src/h/SportsWear.h
+++ Teams/Team№5/src/h/SportsWear.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "ClothingItem.h"
 class SportsWear : public ClothingItem {
-	
+	String sport;
 public:
 	SportsWear(const String& name, const double price, const size_t numAvailable);
-	
+	void setSport(const String&);
 	virtual Type getType() const override;
 	virtual const char* typeToStr() const override;
 	virtual ClothingItem* clone() const override;
